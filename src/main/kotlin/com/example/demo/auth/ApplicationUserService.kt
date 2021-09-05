89ec7dc8601101f62ec615c5a53649a3349f6280
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-09-05 15:51:28 +0900

    feat: add qualifier

--- src/main/kotlin/com/example/demo/auth/ApplicationUserService.kt
+++ src/main/kotlin/com/example/demo/auth/ApplicationUserService.kt
@@ -1,12 +1,16 @@
 package com.example.demo.auth
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
 
+@Service
 class ApplicationUserService(
-    private val applicationUserDao: ApplicationuserDao // これを変えられる
+    @Qualifier("fake") private val applicationUserDao: ApplicationUserDao // これを変えられる
 ) : UserDetailsService {
+    @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(username: String): UserDetails {
         return applicationUserDao.selectApplicationUserByUsername(username) ?: throw UsernameNotFoundException(
             "User $username does not exist"
